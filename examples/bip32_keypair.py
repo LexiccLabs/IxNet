086baeef2f31965a5f10a7370df1c5e055592e3a
gimre <g.imre@protonmail.com>
2021-03-09 17:43:43 +0100

    examples

---
+++ examples/bip32_keypair.py
@@ -0,0 +1,17 @@
+from symbolchain.core.Bip32 import Bip32
+from symbolchain.core.facade.SymFacade import SymFacade
+
+def main():
+    facade = SymFacade('public_test')
+
+    bip = Bip32(facade.BIP32_CURVE_NAME)
+    root_node = bip.from_mnemonic('axis buzz cycle dynamic eyebrow future gym hybrid ivory just know lyrics', 'correcthorsebatterystaple')
+
+    child = root_node.derive_path([44, SymFacade.BIP32_COIN_ID, 0, 0, 0])
+    keyPair = facade.bip32_node_to_key_pair(child)
+
+    print(' * private key: {}'.format(keyPair.private_key))
+    print(' *  public key: {}'.format(keyPair.public_key))
+
+if __name__ == '__main__':
+    main()
---
+++ examples/transaction_descriptor.py
@@ -0,0 +1,30 @@
+from binascii import unhexlify
+from symbolchain.core.CryptoTypes import PrivateKey
+from symbolchain.core.sym.KeyPair import KeyPair
+from symbolchain.core.sym.Network import Address
+from symbolchain.core.facade.SymFacade import SymFacade
+from symbolchain.core.sym.TransactionFactory import TransactionFactory
+
+def main():
+    facade = SymFacade('public_test')
+    keyPair = KeyPair(PrivateKey(bytes(range(1, 33))))
+
+    factory = TransactionFactory(facade.network)
+    transaction = factory.create_from_descriptor({
+        'type': 'transfer',
+        'signerPublicKey': keyPair.public_key.bytes,
+        'recipientAddress': Address('NBQMNBY7Z3YTT2V7Y32CKSVQPJ3YDAD4RLQJEEI').bytes,
+        'fee': 0,
+        'deadline': 1,
+        'message': 'V belom plashche s krovavym podboyem, sharkayushchey kavaleriyskoy pokhodkoy,'.encode('utf-8'),
+        'mosaics': [
+            (0x4F8E3FB75C77C83E, 12345_000000),
+            (0x4F8E3FB75C77C83E, 10)
+        ]
+    })
+
+    print(transaction)
+
+
+if __name__ == '__main__':
+    main()
