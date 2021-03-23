cc9a7e23ea525fbfac193dd5b71e2ab268a773b6
gimre <g.imre@protonmail.com>
2021-03-23 08:48:29 +0100

    show address as well

--- examples/bip32_keypair.py
+++ examples/bip32_keypair.py
@@ -7,11 +7,13 @@ def main():
     bip = Bip32(facade.BIP32_CURVE_NAME)
     root_node = bip.from_mnemonic('axis buzz cycle dynamic eyebrow future gym hybrid ivory just know lyrics', 'correcthorsebatterystaple')
 
-    child = root_node.derive_path([44, SymFacade.BIP32_COIN_ID, 0, 0, 0])
+    child = root_node.derive_path([44, facade.BIP32_COIN_ID, 0, 0, 0])
     keyPair = facade.bip32_node_to_key_pair(child)
 
     print(' * private key: {}'.format(keyPair.private_key))
     print(' *  public key: {}'.format(keyPair.public_key))
+    address = facade.network.public_key_to_address(keyPair.public_key)
+    print(' *     address: {}'.format(address))
 
 if __name__ == '__main__':
     main()
